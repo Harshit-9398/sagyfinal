6198ad1d4b7c603a22b1c72b182741005df062b4
fanaticabhi <abhiyogi999@gmail.com>
2018-03-01 22:27:29 +0530

    completed splash,login,navdrawer

--- app/src/main/java/com/example/abhi/sagyfinal/login.java
+++ app/src/main/java/com/example/abhi/sagyfinal/login.java
@@ -1,20 +1,72 @@
 package com.example.abhi.sagyfinal;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeImageTransform;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 public class login extends AppCompatActivity {
-
-    TextView t1;
+    EditText username,password;
+    TextView not_registered_yet;
+    CheckBox keep_me_logedin;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        t1=(TextView)findViewById(R.id.t1);
-        t1.setOnClickListener(new View.OnClickListener() {
+        not_registered_yet=(TextView)findViewById(R.id.t1);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        keep_me_logedin=(CheckBox)findViewById(R.id.keep_me_logedin);
+        b1=(Button)findViewById(R.id.login_button);
+
+
+
+       b1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+
+               String username1=username.getText().toString();
+               String password1=password.getText().toString();
+
+               if(username1.isEmpty())
+               {
+                   username.setError("enter valid username");
+                   username.requestFocus();
+                   return;
+               }
+               if(password1.length()<6)
+               {
+                   password.setError("min length 6 chars");
+                   password.requestFocus();
+                   return;
+               }
+
+               if(keep_me_logedin.isChecked()==true)
+               {
+                   SharedPreferences sp=getSharedPreferences("userinfo",MODE_PRIVATE);
+                   SharedPreferences.Editor e=sp.edit();
+                   e.putString("username",username1);
+                   e.putString("password",password1);
+                   e.commit();
+               }
+
+               Intent i=new Intent(login.this,home_page.class);
+               startActivity(i);
+           }
+       });
+
+
+
+        not_registered_yet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(login.this,register.class);
--- app/src/main/java/com/example/abhi/sagyfinal/splash.java
+++ app/src/main/java/com/example/abhi/sagyfinal/splash.java
@@ -1,11 +1,14 @@
 package com.example.abhi.sagyfinal;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class splash extends AppCompatActivity {
 
+    String saved_username;
+    String saved_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +18,9 @@ public class splash extends AppCompatActivity {
             public void run(){
                 try{
                     sleep(3000);
-                    Intent i=new Intent(splash.this,home_page.class);
-                    startActivity(i);
+
+                    userinfo();
+
 
                 }
                 catch(Exception e)
@@ -29,4 +33,22 @@ public class splash extends AppCompatActivity {
         th.start();
 
     }
-}
+
+    public void userinfo()
+    {
+        SharedPreferences sp=getSharedPreferences("userinfo",MODE_PRIVATE);
+        saved_username=sp.getString("username",null);
+        saved_password=sp.getString("password",null);
+
+        if(saved_username!=null&&saved_password!=null)
+        {
+            Intent i=new Intent(splash.this,home_page.class);
+            startActivity(i);
+        }
+        else {
+            Intent i=new Intent(splash.this,login.class);
+            startActivity(i);
+        }
+        }
+    }
+
